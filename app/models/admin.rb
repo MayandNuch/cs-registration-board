765d715f74e199a76d590fbf73429a37542f20b8
May <calopc.canova@gmail.com>
2017-03-26 16:33:45 +0700

    add validation to admin student teacher model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,8 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates_presence_of :firstname,:lastname
   devise :database_authenticatable, :registerable,
         :rememberable,:validatable
+
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,7 @@
 class Teacher < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates_presence_of :firstname,:lastname,:teacher_id
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
 
