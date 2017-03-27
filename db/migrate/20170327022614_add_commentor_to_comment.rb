ce683d7857c5ffec67c6d02d3717ce6435408bdf
May <calopc.canova@gmail.com>
2017-03-27 09:38:27 +0700

    last ref commentor still not work

--- db/migrate/20170327022614_add_commentor_to_comment.rb
+++ db/migrate/20170327022614_add_commentor_to_comment.rb
@@ -1,5 +1,5 @@
 class AddCommentorToComment < ActiveRecord::Migration[5.0]
   def change
-    add_reference :comments, :commentor, polymorphic: true, index: true
+    add_reference :comments, :commentor, polymorphic: true
   end
 end
